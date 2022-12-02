b10f9613205ee89001618cc6c8be20e13bf37ec6
inmaculadarakel <inmaculadarakel@users.noreply.github.com>
2022-12-02 11:43:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
-streamlit.title("Breakfast Favorites")
+streamlit.title("My Mom's New Healthy Diner")
 
+
+streamlit.header("Breakfast Favorites")
 streamlit.text ('🥣 Omega 3 & Blueberry Oatmel')
 streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
